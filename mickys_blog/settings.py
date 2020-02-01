ff60df11ef68d93a21fb09f2a500436406c7abdf
Micky Kumar <55264508+mickykumar1@users.noreply.github.com>
2020-02-01 12:02:18 -0500

    update: - The Django admin is functional and can be accessed by the instructor -configured test suite and a test for the index view -1ocal and Heroku PostgreSQL databases!

--- mickys_blog/settings.py
+++ mickys_blog/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,10 +74,9 @@ WSGI_APPLICATION = 'mickys_blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='postgres://postgres:postgres@127.0.0.1:5432/mickys_blog'
+    )
 }
 
 
