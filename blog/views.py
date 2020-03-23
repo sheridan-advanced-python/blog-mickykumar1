4fa8f48420a930372ab602d58079a4303e95bfc6
Micky Kumar <55264508+mickykumar1@users.noreply.github.com>
2020-03-22 22:07:37 -0400

    Assignment 4 - update views files

--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,14 @@ def home(request):
     # Get last 10 posts
     latest_posts = models.Post.objects.published().order_by('-published')[:10]
     authors = models.Post.objects.get_authors()
+    get_posts = models.Post.objects.return_10_post_with_the_most_comments()[:10]
+    get_topics = models.Topic.objects.return_10_topics_with_the_most_post()[:10]
 
     context = {
         'authors': authors,
         'latest_posts': latest_posts
+        'get_posts': get_posts,
+        'get_topics': get_topics
     }
 
     return render(request, 'blog/home.html', context)
--- mickys_blog/views.py
+++ mickys_blog/views.py
@@ -1,8 +1,7 @@
-# mysite/views.py
+# micky's Blog/views.py
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Micky\'s Blog!')
-    return HttpResponse('Hello world!')
+    return HttpResponse("Hello world! Welcome to Micky\'s Blog!")
